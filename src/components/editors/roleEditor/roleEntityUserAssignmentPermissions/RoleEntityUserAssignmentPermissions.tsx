7a8ff04fdb91ede41ada9cc56d9999bfbb6af0c7
Hamza <hamza.khalifa.clashingames@gmail.com>
2024-01-14 03:47:56 +0100

    Renaming files

--- src/components/editors/roleEditor/roleEntityUserAssignmentPermissions/RoleEntityUserAssignmentPermissions.tsx
+++ src/components/editors/roleEditor/roleEntityUserAssignmentPermissions/RoleEntityUserAssignmentPermissions.tsx
@@ -9,7 +9,7 @@ import Checkbox from "../../../checkbox";
 import SearchInput from "../../../searchInput";
 import { IRole } from "../../../../store/slices/roleSlice";
 
-import useStyles from "./RoleEntityUserAssignmentPermissions.styles";
+import useStyles from "./roleEntityUserAssignmentPermissions.styles";
 
 interface IRoleEntityUserAssignmentPermissionsProps {
   modelId?: string;
