4783fac3e7acb5b0a97e0d73e3d685ad6d2dfeff
Hamza <hamza.khalifa.clashingames@gmail.com>
2024-03-18 01:41:18 +0100

    ecommerce addon styles

--- src/components/appComponents/editors/entityEditor/entityEditorEcommerceAddons/entityEditorEcommerceAddons.styles.ts
+++ src/components/appComponents/editors/entityEditor/entityEditorEcommerceAddons/entityEditorEcommerceAddons.styles.ts
@@ -1,6 +1,11 @@
 import { createUseStyles } from "react-jss";
 import { ITheme } from "roottypes";
 
-const useStyles = createUseStyles((theme: ITheme) => ({}));
+const useStyles = createUseStyles((theme: ITheme) => ({
+  entityEditorEcommerceAddonsContainer: {
+    display: "flex",
+    flexDirection: "column",
+  },
+}));
 
 export default useStyles;
